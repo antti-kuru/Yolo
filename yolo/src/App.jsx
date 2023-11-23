86053c6f5bb9eb5579d4a78925153e362f8f548b
Antti Kuru <antti.i.kuru@gmail.com>
2023-11-23 12:08:34 +0200

    Added ability to give proposals

--- yolo/src/App.jsx
+++ yolo/src/App.jsx
@@ -10,7 +10,10 @@ const App = () => {
   const [bad, setBad] = useState(0)
   const [all, setAll] = useState(0)
   const [values, setValue] = useState([])
+
+  const [proposals, setProposals] = useState([])
   
+  const [newProposal, setNewProposal] = useState('')
 
   const handleGoodClick = () => {
     const updatedGood = good + 1
@@ -32,16 +35,47 @@ const App = () => {
     setValue(values.concat(-1))
   }
 
+
+
+  const addProposal = (event) => {
+    event.preventDefault()
+    const proposalObject = {
+        name: newProposal
+    }
+    setProposals(proposals.concat(proposalObject))
+  }
+
+  const handleProposalChange = (event) => {
+    console.log(event.target.value)
+    setNewProposal(event.target.value)
+  }
+
   return (
     <div>
       <h1>Give feedback to YOLO</h1>
       <Button handleClick={handleGoodClick} text='good' className='good'/>
       <Button handleClick={handleNeutralClick} text='neutral' className='neutral' />
       <Button handleClick={handleBadClick} text='bad' className='bad'/>
-
+      
+      <form onSubmit={addProposal}>
+        <div>
+          proposal: <input value={newProposal} onChange={handleProposalChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
 
       <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} all={all} values={values}  />
+
+      <h3>proposed foods</h3>
+      <div>
+          {proposals.map(proposal =>
+            <li key={proposal.name}>
+              {proposal.name}</li> 
+            )}
+      </div>
     </div>
   )
 }
